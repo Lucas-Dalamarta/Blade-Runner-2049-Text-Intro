5c66042b8fedd889b8560cceacc6275999505173
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-11-27 00:22:17 -0300

    Removed font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,11 +11,6 @@ const MyApp = ({ Component, pageProps }) => {
         <link rel="apple-touch-icon" href="/lamp-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="Blade runner 2049's intro text" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <GlobalStyles />
