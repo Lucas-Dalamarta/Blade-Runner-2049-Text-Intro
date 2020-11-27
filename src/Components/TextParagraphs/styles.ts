1e1de40750ed730785435a03b23534f8889a7b2a
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-11-26 21:50:20 -0300

    Added letter-spacing

--- src/Components/TextParagraphs/styles.ts
+++ src/Components/TextParagraphs/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const Paragraph = styled.p`
   font-family: "Lato", sans-serif;
   font-size: 2rem;
+  font-weight: lighter;
+  letter-spacing: 3px;
 
   + p {
     margin-top: 40px;
